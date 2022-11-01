74d79dc64a9c9df8dff985ecd4aa52cecf493a9b
Cole Hansen <99274668+ProfessionalGooseChaser@users.noreply.github.com>
2022-11-01 15:50:23 -0400

    Backlight.py v1.01
    I changed my original function so that it was recursive and that seems to have worked, but it still takes forever to calculate everything.

--- Backlight.py
+++ Backlight.py
@@ -9,7 +9,7 @@ start = time.time()
 SCREEN = (1440, 900)
 _LIGHTS = []
 
-def average2(minX, minY, rangeX, rangeY, LIGHTS):
+def average2(minX, minY, rangeX, rangeY, LIGHTS, TIME):
     sqr = ImageGrab.grab(bbox = (minX, minY, minX + rangeX, minY + rangeY))
     px = sqr.load()
     totalRGB = (0, 0, 0)
@@ -18,6 +18,11 @@ def average2(minX, minY, rangeX, rangeY, LIGHTS):
             totalRGB = tuple(map(sum, zip(px[x,y], totalRGB))) 
     rgb = tuple(ti/(rangeX * rangeY) for ti in totalRGB)
     LIGHTS.append(rgb)
+    print(time.time() - TIME)
+    while(len(LIGHTS) < 106):
+        average2(minX, minY, rangeX, rangeY, LIGHTS, time.time())
+    return 0
+
 
 lights = []
 for i in range(16): #90pixels for range 
@@ -36,31 +41,21 @@ start2 = time.time()
 
 
 #Creates threads for each light, class functions suck so I did this and multithreaded it instead
-threads = [Thread(target=average2, args=(n[0], n[1], n[2], n[3], _LIGHTS)) for n in lights]
+threads = [Thread(target=average2, args=(n[0], n[1], n[2], n[3], _LIGHTS, time.time())) for n in lights]
+
 [t1.start() for t1 in threads]
+[t2.join() for t2 in threads]
 
 #just needed to see some time calues
 end = time.time()
 print(str(start2-start))
 print(str(end-start2)) #takes regularly 10 seconds to boot
 
-queue = Queue()
-#10 iterations
-count = 0
-while(count < 10):
-    count += 1
-    start3 = time.time()
-    #only runs the first time, and then stops
-    for i in range(len(threads)):
-        queue.put(i)
-    if __name__ == '__main__':
-        [t3.join() for t3 in threads]
-    #send values to board
-    print(time.time() - start3) # this time is always within .001 seconds, yeah but it's not doing it again
-    _LIGHTS = []
-    print(len(_LIGHTS))
+print(len(_LIGHTS))
+print(_LIGHTS[0])
+print(_LIGHTS[53])
 
-   
+
 
 #https://docs.python.org/3/library/asyncio-eventloop.html
 #https://stackoverflow.com/questions/45609784/control-a-loop-in-a-thread-class-python
